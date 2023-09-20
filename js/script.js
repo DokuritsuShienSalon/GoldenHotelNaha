580ce3cc2aff6e650293a349a4f557ef4daa4307
NSK-coding <coding@topaz.matrix.jp>
2023-09-20 22:50:19 +0900

    menu implemented

--- js/script.js
+++ js/script.js
@@ -48,3 +48,75 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
 });
+
+
+
+
+
+
+var nowUrl, nowHash, snsData = [], wW, wH, cW, tW, pW, dH, slide0, slideFlg0 = false, openingFlg = false, menuFlg = false, popupFlg = false, thanksFlg = false;
+
+
+
+
+function menuAnimation() {
+  if (menuFlg == false) {
+      menuFlg = true;
+      if ($(".header_button").hasClass("header_button_fix")) {
+          $(".header_button").addClass("header_button_pause")
+      }
+      $(".header_button").addClass("header_button_on");
+      $(".menu").removeClass("none");
+      $(".menu").removeClass("menu_off");
+      $(".menu").addClass("menu_on")
+  } else {
+      menuFlg = false;
+      $(".header_button").removeClass("header_button_on");
+      $(".menu").removeClass("menu_on");
+      $(".menu").addClass("menu_off");
+      if ($(".header_button").hasClass("header_button_fix")) {
+          $(".header_button").delay(250).queue(function() {
+              $(this).removeClass("header_button_pause").dequeue()
+          })
+      } else {
+          if ($(window).scrollTop() > wH) {
+              $(".header_button").delay(250).queue(function() {
+                  $(this).removeClass("header_button_nofix");
+                  $(this).addClass("header_button_fix").dequeue()
+              })
+          }
+      }
+      $(".menu").delay(500).queue(function() {
+          $(this).addClass("none").dequeue()
+      })
+  }
+}
+
+
+$(document).on("click", ".header_button", menuAnimation);
+
+$(document).on("mouseenter", ".pc .menu_list", function() {
+  if ($(this).hasClass("menu_list_on") == false) {
+      $(this).removeClass("menu_list_off");
+      $(this).addClass("menu_list_on")
+  }
+});
+$(document).on("mouseleave", ".pc .menu_list", function() {
+  if ($(this).hasClass("menu_list_on")) {
+      $(this).removeClass("menu_list_on");
+      $(this).addClass("menu_list_off")
+  }
+});
+
+$(document).on("touchstart", ".menu_list", function() {
+  if ($(this).hasClass("menu_list_on") == false) {
+      $(this).removeClass("menu_list_off");
+      $(this).addClass("menu_list_on")
+  }
+});
+$(document).on("touchend", ".menu_list", function() {
+  if ($(this).hasClass("menu_list_on")) {
+      $(this).removeClass("menu_list_on");
+      $(this).addClass("menu_list_off")
+  }
+});
