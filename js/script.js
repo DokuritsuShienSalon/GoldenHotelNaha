c17a43ecc6882bb15fd90be4b30257504b81c8de
NSK-coding <coding@topaz.matrix.jp>
2023-10-09 19:37:08 +0900

    flocss final

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@ var menuFlg = false;
 function menuAnimation() {
   if (menuFlg == false) {
       menuFlg = true;
-      $(".header_button").addClass("header_button_on");
-      $(".menu").removeClass("u-none");
-      $(".menu").removeClass("menu_off");
-      $(".menu").addClass("menu_on")
+      $(".c-hamburger").addClass("js-hamburger-on");
+      $(".p-menu").removeClass("js-menu-off ");
+      $(".p-menu").removeClass("u-none");
+      $(".p-menu").addClass("js-menu-on")
   } else {
       menuFlg = false;
-      $(".header_button").removeClass("header_button_on");
-      $(".menu").removeClass("menu_on");
-      $(".menu").addClass("menu_off");
-      $(".menu").delay(500).queue(function() {
+      $(".c-hamburger").removeClass("js-hamburger-on");
+      $(".p-menu").removeClass("js-menu-on");
+      $(".p-menu").addClass("js-menu-off ");
+      $(".p-menu").delay(500).queue(function() {
           $(this).addClass("u-none").dequeue()
       })
   }
 }
 
-$(document).on("click", ".header_button", menuAnimation);
+$(document).on("click", ".c-hamburger", menuAnimation);
