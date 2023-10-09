5fc31e223b08c04edba85bac28e479feaf5cce70
NSK-coding <coding@topaz.matrix.jp>
2023-10-09 17:39:36 +0900

    hamburger

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var wH, menuFlg = false;
+var menuFlg = false;
 
 function menuAnimation() {
   if (menuFlg == false) {
       menuFlg = true;
-      if ($(".header_button").hasClass("header_button_fix")) {
-          $(".header_button").addClass("header_button_pause")
-      }
       $(".header_button").addClass("header_button_on");
       $(".menu").removeClass("u-none");
       $(".menu").removeClass("menu_off");
@@ -17,49 +14,10 @@ function menuAnimation() {
       $(".header_button").removeClass("header_button_on");
       $(".menu").removeClass("menu_on");
       $(".menu").addClass("menu_off");
-      if ($(".header_button").hasClass("header_button_fix")) {
-          $(".header_button").delay(250).queue(function() {
-              $(this).removeClass("header_button_pause").dequeue()
-          })
-      } else {
-          if ($(window).scrollTop() > wH) {
-              $(".header_button").delay(250).queue(function() {
-                  $(this).removeClass("header_button_nofix");
-                  $(this).addClass("header_button_fix").dequeue()
-              })
-          }
-      }
       $(".menu").delay(500).queue(function() {
           $(this).addClass("u-none").dequeue()
       })
   }
 }
 
-
 $(document).on("click", ".header_button", menuAnimation);
-
-$(document).on("mouseenter", ".pc .menu_list", function() {
-  if ($(this).hasClass("menu_list_on") == false) {
-      $(this).removeClass("menu_list_off");
-      $(this).addClass("menu_list_on")
-  }
-});
-$(document).on("mouseleave", ".pc .menu_list", function() {
-  if ($(this).hasClass("menu_list_on")) {
-      $(this).removeClass("menu_list_on");
-      $(this).addClass("menu_list_off")
-  }
-});
-
-$(document).on("touchstart", ".menu_list", function() {
-  if ($(this).hasClass("menu_list_on") == false) {
-      $(this).removeClass("menu_list_off");
-      $(this).addClass("menu_list_on")
-  }
-});
-$(document).on("touchend", ".menu_list", function() {
-  if ($(this).hasClass("menu_list_on")) {
-      $(this).removeClass("menu_list_on");
-      $(this).addClass("menu_list_off")
-  }
-});
